8e4bece7461aa373992b68e475ef26507649e156
brennoflorencio <brenno.florencio@aec.com.br>
2022-09-16 16:57:30 -0300

    criada toda a estrutura do backend

---
+++ src/Data/Context/MySqlContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Infra.Data.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(new UserMap().Configure);
+        }
+    }
+}
---
+++ src/Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+
+namespace Domain.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        void Insert(TEntity obj);
+
+        void Update(TEntity obj);
+
+        void Delete(int id);
+
+        IList<TEntity> Select();
+
+        TEntity Select(int id);
+    }
+}
